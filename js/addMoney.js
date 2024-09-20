8efe41e724cd4fcaba765a2adc1b829a298c1ccc
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-09-20 23:33:17 +0600

    added tranjection js programe

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,10 +1,6 @@
 // add money btn
 document.getElementById('addMoney-btn').addEventListener('click', function (e) {
     e.preventDefault();
-    // const inputValue = getAllInputFieldValueById();
-    // console.log(inputValue);
-
-
     const addMoney = getAllInputFieldValueById('amount-input');
     const addMoneyPin = getAllInputFieldValueById('addMoney-pin');
     let mainBalance = getAllInnerTextById('main-balance');
@@ -16,22 +12,14 @@ document.getElementById('addMoney-btn').addEventListener('click', function (e) {
     } else {
         alert('full fuill all the requriments')
     }
-
 })
 
-// first cash out btn
-
+// first add money btn
 document.getElementById('first-addMoney-btn').addEventListener('click', function () {
-    const hiddenAddMoney = getTagsById('hidden-addMoney-btn');
-    const removeClass1 = hiddenAddMoney.classList.remove('hidden');
-    const hiddenCashOutMoney = getTagsById('hidden-cash-out-btn');
-    const addClass1 = hiddenCashOutMoney.classList.add('hidden');
-    const tranjectionText = getTagsById('tranjection-text');
-    const addClass2 = tranjectionText.classList.add('hidden');
+    const hiddenAddMoneySection = getTagsById('hidden-addMoney-btn')
 })
 
 // cash out btn
-
 document.getElementById('cashOut-btn').addEventListener('click', function (event) {
     event.preventDefault();
     const cashOutAmmount = getAllInputFieldValueById('CashOutamount-input');
@@ -50,22 +38,13 @@ document.getElementById('cashOut-btn').addEventListener('click', function (event
     }
 })
 
-
 // first cash out btn
-
 document.getElementById('first-cashOut-btn').addEventListener('click', function () {
-    const hiddenBtn = getTagsById('hidden-cash-out-btn');
-    const removeClass = hiddenBtn.classList.remove('hidden');
-    const firstAddMoneyBtn = getTagsById('hidden-addMoney-btn');
-    const addClass1 = firstAddMoneyBtn.classList.add('hidden');
-    const tranjectionText = getTagsById('tranjection-text');
-    const addClass2 = tranjectionText.classList.add('hidden');
+    const hiddenCashOutSection = getTagsById('hidden-cash-out-btn')
 })
 
 // tranjection
-
 document.getElementById('tranjection-btn').addEventListener('click', function () {
     const tranjectionText = getTagsById('tranjection-text');
-    const removeClass = tranjectionText.classList.remove('hidden');
 
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,21 +1,21 @@
 // innerHtml
-function getAllInputFieldValueById (id){
-
-const findIdValue = parseFloat(document.getElementById(id).value);
-
-return findIdValue;
-
+function getAllInputFieldValueById(id) {
+    const findIdValue = parseFloat(document.getElementById(id).value);
+    return findIdValue;
 }
 
 // innerText
-function getAllInnerTextById (id){
+function getAllInnerTextById(id) {
     const findIdValue = parseFloat(document.getElementById(id).innerText);
     return findIdValue;
 }
 
 // first btn 
 
-function getTagsById(id){
-    const findTags = document.getElementById(id)
+function getTagsById(id) {
+    document.getElementById('hidden-addMoney-btn').classList.add('hidden')
+    document.getElementById('hidden-cash-out-btn').classList.add('hidden')
+    document.getElementById('tranjection-text').classList.add('hidden')
+    const findTags = document.getElementById(id).classList.remove('hidden');
     return findTags;
 }

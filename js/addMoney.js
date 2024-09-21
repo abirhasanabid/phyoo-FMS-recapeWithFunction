955016b70e371992344f37a205d456d01b0c1b9e
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-09-21 11:21:00 +0600

    added some code

--- js/addMoney.js
+++ js/addMoney.js
@@ -5,10 +5,13 @@ document.getElementById('addMoney-btn').addEventListener('click', function (e) {
     const addMoneyPin = getAllInputFieldValueById('addMoney-pin');
     let mainBalance = getAllInnerTextById('main-balance');
 
-
     if (addMoneyPin === 5555) {
         const newMainBalance = mainBalance + addMoney;
         document.getElementById('main-balance').innerText = newMainBalance;
+        const p = document.createElement('p');
+        p.innerText=`addMoney : ${addMoney} tk.  New Balance : ${newMainBalance}`;
+        document.getElementById('dynamic-tranjection-div').appendChild(p);
+    
     } else {
         alert('full fuill all the requriments')
     }
@@ -29,9 +32,17 @@ document.getElementById('cashOut-btn').addEventListener('click', function (event
     if (cashOutPin === 5555) {
         const newBalance = mainBalance - cashOutAmmount;
         document.getElementById('main-balance').innerText = newBalance;
+        const p = document.createElement('p');
+        p.innerText=`Cash Out Amount : ${cashOutAmmount} .tk New Balance : ${newBalance}`;
+
+        document.getElementById('dynamic-tranjection-div').appendChild(p);
+
+
         if (newBalance < 0) {
             document.getElementById('main-balance').innerText = 'Invalid Number';
             document.getElementById('balance-text-id').innerHTML = '';
+
+           
         }
     } else {
         alert('ful-fill all the requriements');
